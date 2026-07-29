01c13c836d2a4fabd02fddc0cd672fa54bdc45f7
lisasteiger <75636934+lisasteiger@users.noreply.github.com>
2026-07-29 21:21:50 +0200

    s

--- projekt.js
+++ projekt.js
@@ -4,17 +4,12 @@ window.addEventListener("scroll", () => {
   const nav = document.getElementById("navibereich");
 
   if (window.innerWidth < 600) {
-    nav.classList.add("blur");
+    nav.style.opacity = "0";
 
-    // Timer zurücksetzen
     clearTimeout(scrollTimeout);
 
-    // Nach 200ms Inaktivität wieder scharf stellen
     scrollTimeout = setTimeout(() => {
-      nav.classList.remove("blur");
+      nav.style.opacity = "1";
     }, 200);
-  } else {
-    // Sicherheitshalber: Effekt oberhalb 600px entfernen
-    nav.classList.remove("blur");
   }
 });
